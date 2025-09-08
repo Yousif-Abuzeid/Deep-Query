286bac44b8495ca73db6d35e6f82504cd439fcca
Youssef-Abuzeid <yousifh991@gmail.com>
2025-09-08 19:25:08 +0300

    .

---
+++ src/models/ChunkModel.py
@@ -0,0 +1,59 @@
+from pymongo import InsertOne
+
+from .BaseDataModel import BaseDataModel
+from .db_schemas import DataChunk
+from .enums.DataBaseEnum import DataBaseEnum
+
+
+class ChunkModel(BaseDataModel):
+    def __init__(self, db_client: object):
+        super().__init__(db_client)
+        self.collection = self.db_client[DataBaseEnum.COLLECTION_CHUNK_NAME.value]
+
+    @classmethod
+    async def create_instance(cls, db_client: object):
+        instance = cls(db_client)
+        await instance.init_collection()
+        return instance
+    
+    async def init_collection(self):
+        all_collections = await self.db_client.list_collection_names()
+        if DataBaseEnum.COLLECTION_CHUNK_NAME.value not in all_collections:
+            self.collection = self.db_client[DataBaseEnum.COLLECTION_CHUNK_NAME.value]
+            indexes = DataChunk.get_indexes()
+            for index in indexes:
+                await self.collection.create_index(
+                    index['key'],
+                    name=index['name'],
+                    unique=index['unique']
+                )
+    async def create_chunk(self, data_chunk: DataChunk) -> DataChunk:
+        result = await self.collection.insert_one(
+            data_chunk.dict(by_alias=True, exclude_unset=True)
+        )
+        data_chunk._id = result.inserted_id
+        return data_chunk
+
+    async def get_chunk(self, chunk_id: str) -> DataChunk | None:
+        document = await self.collection.find_one({"_id": chunk_id})
+        if document:
+            return DataChunk(**document)
+        return None
+
+    async def insert_many_chunks(self, chunks: list, batch_size: int = 100):
+        for i in range(0, len(chunks), batch_size):
+            batch = chunks[i : i + batch_size]
+            operations = [
+                InsertOne(chunk.dict(by_alias=True, exclude_unset=True))
+                for chunk in batch
+            ]
+            if operations:
+                await self.collection.bulk_write(operations)
+
+        return len(chunks)
+
+    async def delete_chunks_by_project_id(self, project_id: str):
+        result = await self.collection.delete_many(
+            {"chunk_project_id": project_id}
+        )
+        return result.deleted_count
