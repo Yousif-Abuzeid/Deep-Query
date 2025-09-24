a1f9d8dadc943bf0833bbfd643545c1906993404
Youssef-Abuzeid <yousifh991@gmail.com>
2025-09-25 00:32:20 +0300

    Add chat history

--- src/models/db_schemas/minirag/schemes/project.py
+++ src/models/db_schemas/minirag/schemes/project.py
@@ -1,6 +1,6 @@
 from .minirag_base import SQLAlchemyBase
 from sqlalchemy import Column,Integer,String,DateTime,func
-from sqlalchemy.dialects.postgresql import UUID
+from sqlalchemy.dialects.postgresql import UUID,JSONB
 import uuid
 from sqlalchemy.orm import relationship
 
@@ -9,7 +9,7 @@ class Project(SQLAlchemyBase):
 
     project_id = Column(Integer, primary_key=True, autoincrement=True)
     project_uuid = Column(UUID(as_uuid=True), default=uuid.uuid4, unique=True, nullable=False)
-    
+    chat_history = Column(JSONB, nullable=True)
     created_at = Column(DateTime(timezone=True), server_default=func.now(), nullable=False)
     updated_at = Column(DateTime(timezone=True), onupdate=func.now(), nullable=True)
 
